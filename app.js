ab2d60cc11eb41e80e3d9edb1d8d3eac8528bf64
GeneGenie <wertixvost@gmail.com>
2015-08-13 17:38:48 +0300

    Cors

--- app.js
+++ app.js
@@ -15,7 +15,13 @@ GLOBAL.DB.restore();
 DB.users = DB.users || [];
 DB.posts = DB.posts || [];
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
+  next();
+});
 app.use(bodyParser.json())
 app.use(function (req, res, next) {
     console.log(req.originalUrl)
